7d5559c091fb325736d31efc7e7be6e7e2312247
Hugo Marais <maraishugo.pro@gmail.com>
2023-01-05 20:51:35 +0100

    Added users and login

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 require('dotenv').config()
 require('express-async-errors')
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('movies app')
-})
+mongoose.set('strictQuery', true)
+
+//Connect DB
+const connectDB = require('./db/connect')
+const authenticateUser = require('./middleware/authentication')
+
+//Routers
+const authRouter = require('./routes/auth')
+const moviesRouter = require('./routes/movies')
+
+app.use(express.json())
+
+//Routes
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/movies', authenticateUser, moviesRouter)
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    //await connectDB(process.env.MONGO_URI);
+    await connectDB(process.env.MONGO_URI);
 
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
--- routes/movies.js
+++ routes/movies.js
@@ -1,2 +1,13 @@
 const express = require('express')
 const router = express.Router()
+
+const {getAllMovies, getMovie, createMovie, updateMovie, deleteMovie} = require('../controllers/movies')
+
+router.get('/', getAllMovies)
+router.get('/:id', getMovie)
+router.post('/', createMovie)
+router.post('/:id', updateMovie)
+router.delete('/:id', deleteMovie)
+
+module.exports = router
+
